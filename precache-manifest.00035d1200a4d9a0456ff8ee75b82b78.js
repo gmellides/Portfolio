d55c4e3f0826d4a263639d0840289e5c4467c5f3
Gabriel Mellides <gabmellidis-16@hotmail.com>
2019-02-01 18:05:25 +0200

    Updates

---
+++ precache-manifest.00035d1200a4d9a0456ff8ee75b82b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "37c9be853fe90b69aaae",
+    "url": "/Portfolio/static/js/main.37c9be85.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "37c9be853fe90b69aaae",
+    "url": "/Portfolio/static/css/main.b3fe0c28.chunk.css"
+  },
+  {
+    "revision": "76c4d737ac57dcd28bc8f807d1455da2",
+    "url": "/Portfolio/index.html"
+  }
+];
